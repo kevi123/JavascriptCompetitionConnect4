7beafb0fecda4829dcb07dfaf4faa22fcffd34d3
Kevin Hinojosa <kevi123321@gmail.com>
2018-07-26 17:22:55 -0700

    Not complete, Rough Layout, need to maipulate DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-let loginForm = document.forms.login;
-console.log(document.forms, loginForm);
+// let loginForm = document.forms.login;
+// console.log(document.forms, loginForm);
+// let emailInput = loginForm.email;
 
-let emailInput = loginForm.email;
+
+
+let userInputName1= prompt("Hello player 1, enter your gamerTag");
+let userInputName2= prompt("Player 2, enter your gamerTag");
+
+
+let body = document.querySelectorAll("li")[1]; //Search for the first tag that has <body> and insert it into variable body
+console.log(body);
+
+body.innerHTML="lala";
+body.insertBefore(userInputName1, li)
+
+// document.querySelectorAll("li")[1].innerHTML=userInputName1;
