b2526c15e9a79365fee2723a3af3e9b32f344dbd
Kevin Hinojosa <kevi123321@gmail.com>
2018-10-02 20:14:52 -0700

    changed columns

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,32 @@
 
 
 
-let userInputName1= prompt("Hello player 1, enter your gamerTag");
-let userInputName2= prompt("Player 2, enter your gamerTag");
+// let userInputName1= prompt("Hello player 1, enter your gamerTag");
+// let userInputName2= prompt("Player 2, enter your gamerTag");
 
 
 let body = document.querySelectorAll("li")[1]; //Search for the first tag that has <body> and insert it into variable body
 console.log(body);
 
-body.innerHTML="lala";
-body.insertBefore(userInputName1, li)
+// body.innerHTML="lala";
+// body.insertBefore(userInputName1, li)
 
 // document.querySelectorAll("li")[1].innerHTML=userInputName1;
+
+
+row6=[0,0,0,0,0,0,0];
+row5=[0,0,0,0,0,0,0];
+row4=[0,0,0,0,0,0,0];
+row3=[0,0,0,0,0,0,0];
+row2=[0,0,0,0,0,0,0];
+row1=[0,0,0,0,0,0,0];
+
+
+// document.getElementById("column1").addEventListener("dblclick", sendToGrave1);
+
+console.log(row6);
+console.log(row5);
+console.log(row4);
+console.log(row3);
+console.log(row2);
+console.log(row1);
